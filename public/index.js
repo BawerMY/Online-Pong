e933c9e59718af79a1c2966cd3c186085b989150
Bawer <bawermy123@gmail.com>
2024-09-24 23:35:39 +0200

    initial

---
+++ public/index.js
@@ -0,0 +1,146 @@
+const socket = io("http://localhost:3000");
+
+document.getElementById("create-game-button").addEventListener("click", e => {
+    socket.emit("create-game");
+})
+
+document.getElementById("join-game-button").addEventListener("click", e => {
+    socket.emit("join-game", document.getElementById("game-id-input").value);
+})
+
+socket.on("game-created", gameId => {
+    document.getElementById("create-game-menu").style.display = "block"
+    document.getElementById("game-id").innerText = gameId;
+    document.getElementById("main-menu").style.display = "none";
+})
+
+socket.on("game-not-found", () => {
+    alert("game not found")
+})
+
+socket.on("waiting-for-the-host-to-start-the-game", () => {
+    document.getElementById("join-game-menu").style.display = "flex"
+    document.getElementById("main-menu").style.display = "none";
+})
+
+socket.on("waiting-for-you-to-start-the-game", () => {
+    document.getElementById("start-button").disabled = false
+})
+
+Array.from(document.getElementsByClassName("exit-button")).forEach((button) => {
+    button.addEventListener("click", () => {
+        socket.emit("exit-lobby")
+        document.getElementById("join-game-menu").style.display = "none"
+        document.getElementById("create-game-menu").style.display = "none"
+        document.getElementById("main-menu").style.display = "flex";
+    })
+})
+
+socket.on("game-not-full", () => {
+    document.getElementById("start-button").disabled = true;
+})
+
+document.getElementById("start-button").addEventListener("click", e => {
+    socket.emit("game-start")
+    document.getElementById("create-game-menu").style.display = "none"
+    document.getElementById("score-table").style.display = "block"
+    document.getElementById("exit").style.display = "block"
+    document.getElementById("game-canvas").style.display = "block"
+})
+
+socket.on("game-start", () => {
+    document.getElementById("join-game-menu").style.display = "none";
+    document.getElementById("score-table").style.display = "block"
+    document.getElementById("exit").style.display = "block"
+    document.getElementById("game-canvas").style.display = "block"
+})
+
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+
+let positions = {
+    left: 250,
+    right: 250,
+    ball: {
+        x: 500,
+        y: 250
+    }
+}
+
+let score = {
+    p1: 0,
+    p2: 0
+}
+
+playerWidth = 33;
+playerHeight = 100;
+ballRadius = 10
+
+function updateCanvas() {
+    ctx.beginPath()
+    ctx.fillStyle = "#000000"
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#ffffff"
+    ctx.fillRect(1000 - playerWidth, positions.right-playerHeight/2, playerWidth, playerHeight);
+    ctx.fillRect(0, positions.left-playerHeight/2, playerWidth, playerHeight);
+    ctx.arc(positions.ball.x, positions.ball.y, ballRadius, 0, Math.PI * 2, false)
+    ctx.fill()
+    ctx.closePath()
+}
+
+socket.on("game", data => {
+    positions.left = data.firstPlayerY
+    positions.right = data.secondPlayerY
+    positions.ball = data.ballPosition
+    updateCanvas()
+    socket.emit("input", {
+        pressingW: pressingW,
+        pressingS: pressingS,
+    })
+})
+
+socket.on('score', score => {
+    document.getElementById("score-table").innerText = `${score.p1}-${score.p2}`
+})
+
+let pressingW = false
+let pressingS = false
+
+document.addEventListener("keydown", e => {
+    if (e.key === "w" && pressingW !== true) {
+        pressingW = true
+    }
+    if (e.key === "s" && pressingS !== true) {
+        pressingS = true
+    }
+})
+
+document.addEventListener("keyup", e => {
+    if (e.key === "w" && pressingW !== false) {
+        pressingW = false
+    }
+    if (e.key === "s" && pressingS !== false) {
+        pressingS = false
+    }
+})
+
+document.getElementById('exit').addEventListener("click", e => {
+    canvas.style.display = "none";
+    document.getElementById("score-table").style.display = "none"
+    document.getElementById("exit").style.display = "none"
+    document.getElementById("main-menu").style.display = "flex"
+})
+
+socket.on("opponent-disconnected", () => {
+    canvas.style.display = "none"
+    document.getElementById("score-table").style.display = "none"
+    document.getElementById("exit").style.display = "none"
+    document.getElementById("main-menu").style.display = "flex"
+    document.getElementById("join-game-menu").style.display = "none"
+    document.getElementById("create-game-menu").style.display = "none"
+    alert("Opponent disconnected")
+})
+
+document.getElementById("exit").addEventListener("click", e => {
+    socket.emit("exit")
+})
---
+++ server.js
@@ -0,0 +1,222 @@
+const express = require('express');
+const app = express()
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.sendfile('public/index.html');
+});
+
+let lastGameId = 0
+
+class Game {
+    values = {
+        canvasWidth: 1000,
+        canvasHeight: 500,
+        moveSpeed: 5,
+        playerHeight: 100,
+        playerWidth: 33,
+        ballRadius: 10,
+        ballSpeed: 3,
+        ballSpeedIncrease: 0.5
+    }
+    constructor(hostId) {
+        this.firstPlayer = hostId;
+        this.secondPlayer = null;
+        this.score = {
+            p1: 0,
+            p2: 0
+        }
+        this.firstPlayerY = this.values.canvasHeight / 2
+        this.firstPlayerMove = 0
+        this.secondPlayerY = this.values.canvasHeight / 2
+        this.secondPlayerMove = 0
+        this.ballPosition = {
+            x: this.values.canvasWidth / 2,
+            y: this.values.canvasHeight / 2
+        }
+        this.ballDirection = {
+            x: -1,
+            y: -0
+        }
+        this.ballSpeed = 3
+        this.play = (socket, toId) => {
+            setInterval(() => {
+                this.firstPlayerY += this.firstPlayerMove * this.values.moveSpeed
+                this.ballPosition.x += this.ballDirection.x * this.ballSpeed
+                this.ballPosition.y += this.ballDirection.y
+                if (this.firstPlayerY < this.values.playerHeight / 2) this.firstPlayerY = this.values.playerHeight / 2
+                if (this.firstPlayerY > this.values.canvasHeight - this.values.playerHeight / 2) this.firstPlayerY = this.values.canvasHeight - this.values.playerHeight / 2
+                this.secondPlayerY += this.secondPlayerMove * this.values.moveSpeed
+                if (this.secondPlayerY < this.values.playerHeight / 2) this.secondPlayerY = this.values.playerHeight / 2
+                if (this.secondPlayerY > this.values.canvasHeight - this.values.playerHeight / 2) this.secondPlayerY = this.values.canvasHeight - this.values.playerHeight / 2
+                const game = {
+                    firstPlayerY: this.firstPlayerY,
+                    secondPlayerY: this.secondPlayerY,
+                    ballPosition: this.ballPosition,
+                }
+                if (this.ballPosition.x >= this.values.canvasWidth - this.values.playerWidth - this.values.ballRadius) {
+                    if (Math.abs(this.ballPosition.y - this.secondPlayerY) <= this.values.playerHeight / 2) {
+                        const touchValue = this.ballPosition.y - this.secondPlayerY
+                        this.ballDirection.x *= -1
+                        this.ballDirection.y = touchValue / (this.values.playerHeight / 2) * this.ballSpeed
+                        this.ballSpeed += this.values.ballSpeedIncrease
+                    }
+                }
+                if (this.ballPosition.x - this.values.ballRadius <= this.values.playerWidth) {
+                    if (Math.abs(this.ballPosition.y - this.firstPlayerY) <= this.values.playerHeight / 2) {
+                        const touchValue = this.ballPosition.y - this.firstPlayerY
+                        this.ballDirection.x *= -1
+                        this.ballDirection.y = touchValue / (this.values.playerHeight / 2) * this.ballSpeed
+                        this.ballSpeed += this.values.ballSpeedIncrease
+                    }
+                }
+                if (this.ballPosition.y > this.values.canvasHeight - this.values.ballRadius || this.ballPosition.y < this.values.ballRadius) {
+                    this.ballDirection.y *= -1
+                }
+                if (this.ballPosition.x < this.values.ballRadius) {
+                    this.score.p2 += 1
+                    socket.emit("score", this.score)
+                    socket.to(toId).emit("score", this.score)
+                    this.ballPosition = {
+                        x: this.values.canvasWidth / 2,
+                        y: this.values.canvasHeight / 2
+                    }
+                    this.ballDirection = {
+                        x: -1,
+                        y: 0
+                    }
+                    this.ballSpeed = this.values.ballSpeed
+                }
+                if (this.ballPosition.x > this.values.canvasWidth - this.values.ballRadius) {
+                    this.score.p1 += 1
+                    socket.emit("score", this.score)
+                    socket.to(toId).emit("score", this.score)
+                    this.ballPosition = {
+                        x: this.values.canvasWidth / 2,
+                        y: this.values.canvasHeight / 2
+                    }
+                    this.ballDirection = {
+                        x: 1,
+                        y: 0
+                    }
+                    this.ballSpeed = this.values.ballSpeed
+                }
+                if (this.score.p1 === 15) {
+                    if (socket.id === this.firstPlayer) {
+                        socket.emit("game-won", this.score)
+                        socket.to(toId).emit("lost-won", this.score)
+                    } else {
+                        socket.emit("game-lost", this.score)
+                        socket.to(toId).emit("lost-lost", this.score)
+                    }
+                    delete games[io.sockets.sockets.get(this.firstPlayer).playingGameId]
+                    console.log(games)
+                } else if (this.score.p2 === 15) {
+                    if (socket.id === this.firstPlayer) {
+                        socket.emit("game-lost", this.score)
+                        socket.to(toId).emit("game-won", this.score)
+                    } else {
+                        socket.emit("game-won", this.score)
+                        socket.to(toId).emit("game-lost", this.score)
+                    }
+                    delete games[io.sockets.sockets.get(this.firstPlayer).playingGameId]
+                    console.log(games)
+                }
+                socket.emit("game", game)
+                socket.to(toId).emit("game", game)
+            }, 16)
+        }
+    }
+}
+
+let games = {}
+
+io.on('connection', socket => {
+    let playingGameId = null
+    socket.on('create-game', () => {
+        if (playingGameId !== null) {
+            delete games[playingGameId];
+            playingGameId = null
+        }
+        games[lastGameId] = new Game(socket.id)
+        playingGameId = lastGameId
+        lastGameId++
+        socket.emit('game-created', playingGameId);
+    })
+
+    socket.on("join-game", gameId => {
+        if (games[gameId] === undefined) socket.emit('game-not-found');
+        else if (games[gameId].secondPlayer === null) {
+            playingGameId = gameId
+            games[gameId].secondPlayer = socket.id
+            socket.emit('waiting-for-the-host-to-start-the-game');
+            socket.to(games[gameId].firstPlayer).emit("waiting-for-you-to-start-the-game")
+        } else {
+            socket.emit("game-already-full")
+        }
+    })
+
+    socket.on('exit-lobby', () => {
+        if (playingGameId !== null) {
+            if (games[playingGameId].firstPlayer === socket.id) {
+                if (games[playingGameId].secondPlayer !== null) {
+                    socket.to(games[playingGameId].secondPlayer).emit("opponent-disconnected")
+                    io.sockets.sockets.get(games[playingGameId].secondPlayer).playingGameId = null
+                }
+                delete games[playingGameId]
+                playingGameId = null
+            } else if(games[playingGameId].secondPlayer === socket.id) {
+                socket.to(games[playingGameId].firstPlayer).emit("game-not-full")
+                games[playingGameId].secondPlayer = null
+            }
+        }
+    })
+
+    socket.on('exit', () => {
+        if (socket.id === games[playingGameId].firstPlayer) {
+            socket.to(games[playingGameId].secondPlayer).emit("opponent-disconnected")
+            io.sockets.sockets.get(games[playingGameId].secondPlayer).playingGameId = null
+        } else {
+            socket.to(games[playingGameId].firstPlayer).emit("opponent-disconnected")
+            io.sockets.sockets.get(games[playingGameId].firstPlayer).playingGameId = null
+        }
+        delete games[playingGameId]
+        playingGameId = null
+    })
+
+    socket.on('game-start', () => {
+        socket.to(games[playingGameId].secondPlayer).emit("game-start")
+        games[playingGameId].play(socket, games[playingGameId].firstPlayer === socket.id ? games[playingGameId].secondPlayer : games[playingGameId].firstPlayer)
+    })
+
+    socket.on('input', data => {
+        if (games[playingGameId] === undefined) return
+            let player = games[playingGameId].firstPlayer === socket.id ? "firstPlayer" : "secondPlayer"
+        if (data.pressingW) {
+            if (data.pressingS) games[playingGameId][player + "Move"] = 0
+            else games[playingGameId][player + "Move"] = -1
+        }  else if (data.pressingS) games[playingGameId][player + "Move"] = 1
+        else games[playingGameId][player + "Move"] = 0
+    })
+
+    socket.on('disconnect', () => {
+        if (playingGameId) {
+            if (games[playingGameId].firstPlayer === socket.id) {
+                socket.to(games[playingGameId].secondPlayer).emit('opponent-disconnected');
+                io.sockets.sockets.get(games[playingGameId].secondPlayer).playingGameId = null
+            } else {
+                socket.to(games[playingGameId].firstPlayer).emit('opponent-disconnected');
+                io.sockets.sockets.get(games[playingGameId].firstPlayer).playingGameId = null
+            }
+            delete games[playingGameId]
+            // end game
+        }
+    })
+})
+
+server.listen(port, function() {
+    console.log(`Listening on port ${port}`);
+});
